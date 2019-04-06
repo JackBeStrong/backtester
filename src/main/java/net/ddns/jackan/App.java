d85e505f13c96aa2050e2bef4037dc57c763985b
Jack <junzhouan@gmail.com>
2019-04-06 20:05:39 +1100

    Added logs

--- src/main/java/net/ddns/jackan/App.java
+++ src/main/java/net/ddns/jackan/App.java
@@ -1,9 +1,21 @@
 package net.ddns.jackan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
 
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public final static String fileLocation = "src/main/resources/testData/SampleDataAUDUSD20161203.csv";
+
+
     public static void main(String[] args) {
-        CsvReader reader = new CsvReader("src/main/resources/testData/SampleDataAUDUSD20161203.csv");
+
+        logger.debug("App started");
+
+        CsvReader reader = new CsvReader(fileLocation);
+        logger.debug("Data source file loaded: {}", fileLocation);
         for(Bar bar: reader.readBars()){
             System.out.println(bar);
         }
--- src/main/java/net/ddns/jackan/CsvReader.java
+++ src/main/java/net/ddns/jackan/CsvReader.java
@@ -1,5 +1,8 @@
 package net.ddns.jackan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CsvReader {
+
+    private final Logger logger = LoggerFactory.getLogger(CsvReader.class);
 
     private String csvFile;
     private BufferedReader br;
@@ -26,29 +31,38 @@ public class CsvReader {
         ArrayList<Bar> bars = new ArrayList<Bar>();
         try {
             br = new BufferedReader(new FileReader(csvFile));
+            logger.debug("Buffered reader created with file: {}", csvFile);
             while ((line = br.readLine()) != null) {
                 String[] aRow = line.split(csvpliteBy);
                 try {
                     bars.add(new Bar(format1.parse(aRow[0]), Double.parseDouble(aRow[1]), Double.parseDouble(aRow[2])
                             , Double.parseDouble(aRow[3]), Double.parseDouble(aRow[4])));
                 } catch (ParseException e) {
+                    logger.warn("Datetime format not correct. Trying with dd/MM/yyyy with: {},{},{},{},{}", aRow[0], aRow[1],
+                            aRow[2], aRow[3], aRow[4]);
                     try {
                         bars.add(new Bar(format2.parse(aRow[0]), Double.parseDouble(aRow[1]),
                                 Double.parseDouble(aRow[2]), Double.parseDouble(aRow[3]), Double.parseDouble(aRow[4])));
                     } catch (ParseException e1) {
+                        logger.error("Datetime format cannot be parsed: {}", aRow[0]);
                         e1.printStackTrace();
                     }
                 }
             }
         } catch (FileNotFoundException e) {
+            logger.error("Source file not found: {}", csvFile);
             e.printStackTrace();
         } catch (IOException e) {
+            logger.error("Unknown error occurred");
             e.printStackTrace();
         } finally {
+            logger.debug("Trying to close buffer reader.");
             if (br != null) {
                 try {
                     br.close();
+                    logger.debug("Buffer reader closed successfully.");
                 } catch (IOException e) {
+                    logger.error("Failed to close buffer reader.");
                     e.printStackTrace();
                 }
             }
